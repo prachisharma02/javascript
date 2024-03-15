c825418d783702b860fbb6cc6afda343aa885580
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-03-15 11:34:03 +0530

    done with selectors

--- js.js
+++ js.js
@@ -3,3 +3,20 @@ function fade() {
   //$ it works as a selector similar to queryselector or getelementbyid
   //fadetoggle just fades in and out no need of setting long code for fading out
 }
+function change() {
+  // similar to getelemetbytagname
+  // $("div").css("background-color", "orange"); // it will change the color of all div elements background to orange.
+
+  // $("#div2").css("background-color", "orange"); // to chnage only particular div
+  // it will not work for similar ids jquery needs id to be unique but can be possible with class
+
+  // $("#div3,#div1").css("background-color", "green"); // to change multiple divs with diffrenet id
+
+  //$("#div1,li,.para").css("background-color", "purple"); // we can also do this like one is id and other is class or tagname
+
+  // $("div li").fadeToggle(); // to access inside the element also("div >li")
+
+  $("p:first").fadeToggle(); // it does not support furthur like second or third and it will acess the very first p tag in wole document
+
+  $("li:odd").fadeToggle(); // can also work with even
+}
